400699f25c4674fb914a050c7dc851f92f77abf2
Samuelf17 <samuelopes2003@gmail.com>
2026-06-26 20:39:08 -0300

    Conexão com o BD e criação da tela de jogo
    - Criação do arquivo conexaoBD.js que faz a conexão com o BD e contem
    funções de busca exportaveis
    - Criação dos arquivos tela_jogo.html, style_jogo.css e script_jogo.js para o desenvolvimento da tela de jogo

---
+++ js/conexaoBD.js
@@ -0,0 +1,42 @@
+const supabaseUrl = 'https://qogwqralxlrdtewzcaum.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvZ3dxcmFseGxyZHRld3pjYXVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNzg4NDcsImV4cCI6MjA5MjY1NDg0N30.0kOKsfoVK4IimiRdl7xDvHKwE-UB1LaMoreFwhFBpOU';
+
+const supabaseClient = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
+export async function selectIgual(tableName, colunas, atributo, valor) {
+    const { data, error } = await supabaseClient
+        .from(`${tableName}`)
+        .select(`${colunas}`)
+        .eq(`${atributo}`, `${valor}`);
+    if(error){
+        console.error(error);
+        return null;
+    }
+    console.log(data);
+}
+
+export async function selectDif(tableName, colunas, atributo, valor) {
+    const { data, error } = await supabaseClient
+        .from(`${tableName}`)
+        .select(`${colunas}`)
+        .neq(`${atributo}`, `${valor}`);
+    if(error){
+        console.error(error);
+        return null;
+    }
+    console.log(data);
+}
+
+export async function select(tableName, colunas) {
+    const { data, error } = await supabaseClient
+        .from(`${tableName}`)
+        .select(`${colunas}`)
+    if(error){
+        console.error(error);
+        return null;
+    }
+    console.log(data);
+}
---
+++ js/script_jogo.js
@@ -0,0 +1,4 @@
+import * as conexao from './conexaoBD.js';
+
+conexao.select('personagem_jogo', '*');
+
